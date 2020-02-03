2049aaee4d52139176e285c76bfda8c176c45c8e
soleni <k1a3w3a7i1@gmail.com>
2020-02-03 17:05:22 +0300

    clear debug messages

--- src/main.c
+++ src/main.c
@@ -18,27 +18,22 @@ int main(int argc, char **argv){
                 printf("password is: %s\n",generate(size > 8 ? size : 8));
         }
 	if(strcmp(argv[1],"encrypt") == 0){
-		char *temp = getpass("Give password:\n");
+		char *temp;
 
+	        temp = getpass("Give password:\n");
 		char *Pass = malloc(strlen(temp));
 	       	strcpy(Pass , temp);
 
 		temp = getpass("Give key:\n");
-
 		char *Key = malloc(strlen(temp));
 	       	strcpy(Key , temp);
 		
-		printf("Pass : %s , Key : %s\n" , Pass , Key);
 		printf("encrypted pass is:\n");
-		
 		for(int i = 0 ; i < strlen(Pass) ; i ++){
 			printf("%c" , *(encrypt(Pass,Key) + i));
 		}
-		printf("\ncheck :\n");
-		for(int i = 0 ; i < strlen(Pass) ; i ++){
-			printf("%c" , *(encrypt(encrypt(Pass , Key) , Key) + i));
-		}
 		printf("\n");
+
 	}
         printf("finished!\n");
         return 0;
