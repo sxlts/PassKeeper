1aad643935c980bce0475c8ad1a75e66d7c2beba
Eug-VS <eug-vs@keemail.me>
2020-02-08 23:48:20 +0300

    fix: correct strong password test

--- test/generate.test.c
+++ test/generate.test.c
@@ -4,7 +4,7 @@
 
 int main() {
   unit_test(!is_strong("abcd", 4), "abcd is a weak password");
-  unit_test(!is_strong("abc^3Solenji", 12), "abc^3Solenji is a strong password");
+  unit_test(is_strong("abc^3Solenji", 12), "abc^3Solenji is a strong password");
 
   return report();
 }
