1176bdfd96d12a1c5b107fca7c89a55b65650374
Eug-VS <eug-vs@keemail.me>
2020-02-08 15:13:29 +0300

    feat: seperate is_strong function in generate.h

--- src/generate.h
+++ src/generate.h
@@ -1,10 +1,18 @@
+#ifndef LOADED
+#define LOADED
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <stdbool.h>
 #include "sodium.h"
 
 #define FIRST_SYMBOL 33
 #define ALPHABET_SIZE 94
 
+
+bool is_strong(char* password, int size);
+
 char* generate(int size);
+
+#endif
